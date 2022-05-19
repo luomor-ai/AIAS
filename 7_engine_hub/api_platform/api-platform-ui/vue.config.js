2680e28f9a9ef0c39ea655269370a4b9f100210a
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-05-19 16:53:04 +0800

    update

--- 7_engine_hub/api_platform/api-platform-ui/vue.config.js
+++ 7_engine_hub/api_platform/api-platform-ui/vue.config.js
@@ -45,7 +45,7 @@ module.exports = {
         }
       },
       '/predict': {
-        target: 'http://49.232.6.131:8096',
+        target: 'https://ai.luomor.com',
         changeOrigin: true,
         pathRewrite: {
           '^/predict': 'predict'
