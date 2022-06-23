ca25e1856bb685d4ac65c587e9462310d8e97093
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-06-23 11:57:28 +0800

    update

--- 7_engine_hub/api_platform/api-platform-ui/src/views/hub/speech-asr.vue
+++ 7_engine_hub/api_platform/api-platform-ui/src/views/hub/speech-asr.vue
@@ -85,14 +85,21 @@
 
       <el-row>
         <el-col :span="9">
-            <el-button type="primary"
+            <!--<el-button type="primary"
                 @mousedown.native="mouseStart"
                 @mouseup.native="mouseEnd"
                 v-on:touchstart="mouseStart"
                 v-on:touchend="mouseEnd"
-                >{{ form.time }}</el-button>
+                >{{ form.time }}</el-button>-->
+            <el-button type="primary" @click="recOpen">打开录音,请求权限</el-button>
             <p />
-            <span>{{ form.time }}</span>
+            <el-button type="primary" @click="recClose">关闭录音,释放资源</el-button>
+            <p />
+            <el-button type="primary" @click="recStart">录制</el-button>
+            <p />
+            <el-button type="primary" @click="recStop" style="margin-right:80px">停止</el-button>
+            <p />
+            <span>{{ form.tip }}</span>
             <p />
             <audio
                 v-if="form.audioUrl"
@@ -134,6 +141,7 @@ export default {
         result2: '',
         base64Audio: '',
         time: '按住说话(60秒)',
+        tip: '先打开录音，点击录制按钮开始说话',
         audioUrl: '',
         result3: ''
       },
@@ -143,6 +151,15 @@ export default {
       audioFileList: [], // 上传语音列表
       startTime: '', // 语音开始时间
       endTime: '', // 语音结束
+      Rec: Recorder,
+      type: "mp3",
+      bitRate: 16,
+      sampleRate: 16000,
+      rec: 0,
+      duration: 0,
+      powerLevel: 0,
+      recOpenDialogShow: 0,
+      logs: []
     }
   },
   methods: {
@@ -308,6 +325,103 @@ export default {
             console.log(fd)
             // this.uploadFile(fd)
         }
+    },
+    recOpen: function () {
+        var This = this;
+        var rec = this.rec = Recorder({
+            type: This.type
+            , bitRate: This.bitRate
+            , sampleRate: This.sampleRate
+            , onProcess: function (buffers, powerLevel, duration, sampleRate) {
+                This.duration = duration;
+                This.powerLevel = powerLevel;
+            }
+        });
+        This.dialogInt = setTimeout(function () {//定时8秒后打开弹窗，用于监测浏览器没有发起权限请求的情况
+
+        }, 8000);
+        rec.open(function () {
+            console.log("已打开:" + This.type + " " + This.sampleRate + "hz " + This.bitRate + "kbps", 2);
+
+        }, function (msg, isUserNotAllow) {
+            console.log((isUserNotAllow ? "UserNotAllow，" : "") + "打开失败：" + msg, 1);
+        });
+        This.waitDialogClickFn = function () {
+            console.log("打开失败：权限请求被忽略，用户主动点击的弹窗", 1);
+        };
+    },
+    recClose: function () {
+        var rec = this.rec;
+        this.rec = null;
+        if (rec) {
+            rec.close();
+            console.log("已关闭");
+        } else {
+            console.log("未打开录音", 1);
+        }
+        ;
+    },
+    recStart: function () {
+        if (!this.rec || !Recorder.IsOpen()) {
+            console.log("未打开录音", 1);
+            return;
+        }
+        this.rec.start();
+        var set = this.rec.set;
+        console.log("录制中：" + set.type + " " + set.sampleRate + "hz " + set.bitRate + "kbps");
+    },
+    recPause: function () {
+        if (this.rec && Recorder.IsOpen()) {
+            this.rec.pause();
+        } else {
+            console.log("未打开录音", 1);
+        }
+        ;
+    },
+    recResume: function () {
+        if (this.rec && Recorder.IsOpen()) {
+            this.rec.resume();
+        } else {
+            console.log("未打开录音", 1);
+        }
+        ;
+    },
+    recStop: function () {
+        if (!(this.rec && Recorder.IsOpen())) {
+            console.log("未打开录音", 1);
+            return;
+        }
+
+        var This = this;
+        var rec = This.rec;
+        rec.stop(function (blob, duration) {
+            console.log("已录制:", "", {
+                blob: blob
+                , duration: duration
+                , rec: rec
+            });
+            This.form.audioUrl = window.URL.createObjectURL(blob);
+            let fr = new FileReader();
+            fr.onloadend = function (e) {
+                let base64 = e.target.result;
+                const audio1 = base64.substring(base64.indexOf(',') + 1)
+                const data = {
+                    audio: audio1,
+                    audio_format: "mp3",
+                    sample_rate: 16000,
+                    lang: "zh_cn",
+                    punc: 0,
+                }
+                This.fullscreenLoading = true
+                speechAsr(JSON.stringify(data)).then(response => {
+                    This.fullscreenLoading = false
+                    This.form.result3 = response.result
+                })
+            };
+            fr.readAsDataURL(blob);
+        }, function (s) {
+            console.log("录音失败：" + s, 1);
+        });
     }
   }
 }
